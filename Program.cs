9ff89d516593b05677f66c56c6809ebf8b4c8df4
ivannew <ivanenriquesotorodriguez@gmail.com>
2023-11-07 13:16:21 -0700

    primer

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace examenR_IESR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
